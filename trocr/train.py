e3dfef6db9f79e03be223a2dfef5b1bfccf4c6cd
ochwo laban <labanochwo@gmail.com>
2026-08-21 12:12:51 +0300

    made changes

--- trocr/train.py
+++ trocr/train.py
@@ -145,8 +145,8 @@ def main():
         elif cfg.train.preprocess.type is None: 
             tfm = None
         else:
-            raise Exception('Undeined image preprocess method.')          
-        dataset[split] = HTRDataset(split_data, processor, transform=tfm, max_target_length=cfg.model.generation_config.max_target_length)
+            raise Exception('Undeined image preprocess method.')       
+        dataset[split] = HTRDataset(split_data, processor, transform=(tfm if split=='train' else None), max_target_length=cfg.model.generation_config.max_target_length)
     
     training_args = Seq2SeqTrainingArguments(
         predict_with_generate=True,
